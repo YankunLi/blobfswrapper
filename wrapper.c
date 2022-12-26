8fc59b92ef568ad1c84a47e040d886ac68a6bf88
LiYankun <liyankun01@58.com>
2022-12-26 16:57:08 +0800

    wrapper: generate 512kb read/write and 4kb sequence write
    Signed-off-by: LiYankun <liyankun01@58.com>

--- wrapper.c
+++ wrapper.c
@@ -5,11 +5,19 @@
 #include <stdio.h>
 #include <blobfs_wrapper.h>
 #include <sys/timeb.h>
+#include <sys/time.h>
 
-#define GB 1024*1024*1024L
+#define US 1L
+#define MS (1000*US)
+#define S (1000*MS)
+
+#define KB 1024L
+#define MB (1024*KB)
+#define GB (1024*MB)
 
 int main(int argc, char **argv) {
-	struct timeb startTime , endTime;
+        int64_t bench_size = BENCH_SIZE; //unit GB
+	struct timeval startTime , endTime;
         printf("start blobfs wrapper!");
 	if (argc < 4) {
 		fprintf(stderr, "usage: %s <conffile> <bdev name> <cache_size_in_mb>\n", argv[0]);
@@ -25,7 +33,7 @@ int main(int argc, char **argv) {
         }
         fprintf(stdout, "blobfs mount successfully!!\n");
 
-	ftime(&startTime);
+	gettimeofday(&startTime, NULL);
 	if (strcmp(argv[4], "create") == 0) {
 		char *name = (char *) malloc(125);
 		char *prefix =  "testfilename-";
@@ -39,14 +47,15 @@ int main(int argc, char **argv) {
 			}
 		}
 createout:
-	        ftime(&endTime);
+	        gettimeofday(&endTime, NULL);
 		free(name);
-	        fprintf(stdout, "create 10000 file escaped: %d ms\n", (endTime.time-startTime.time)*1000 + (endTime.millitm - startTime.millitm));
+                uint64_t escaped = (endTime.tv_sec - startTime.tv_sec) * S + (endTime.tv_usec - startTime.tv_usec);
+	        fprintf(stdout, "create 10000 file escaped: %d ms\n", escaped/MS);
                 unmount_blobfs();
 		exit(0);
 	}
 
-	ftime(&startTime);
+	gettimeofday(&startTime, NULL);
 	if (strcmp(argv[4], "del") == 0) {
                 char *name = (char *) malloc(125);
                 char *prefix =  "testfilename-";
@@ -60,39 +69,43 @@ createout:
                         }
                 }
 delout:
-                ftime(&endTime);
+                gettimeofday(&endTime, NULL);
 		free(name);
-                fprintf(stdout, "del 10000 file escaped: %d ms\n", (endTime.time-startTime.time)*1000 + (endTime.millitm - startTime.millitm));
+                uint64_t escaped = (endTime.tv_sec - startTime.tv_sec) * S + (endTime.tv_usec - startTime.tv_usec);
+                fprintf(stdout, "del 10000 file escaped: %d ms\n", escaped/MS);
                 unmount_blobfs();
                 exit(0);
         }
 
 	static const int size = 1024*4;
 
-	ftime(&startTime);
 	if (strcmp(argv[4], "write") == 0) {
                 blobfs_file *wfile = NULL;
 		char *filename = "write_data";
-		char data[size];
                 rc = blobfs_open_file(filename, SPDK_BLOBFS_OPEN_CREATE, &wfile);
                 if (rc != 0) {
 			fprintf(stderr, "ERR: blobfs open file %s\n", filename);
 			goto writeout;
 		}
 
+                uint64_t chunk_size = (uint64_t) atol(argv[5]) * KB;
+                char *data = (char *) malloc(chunk_size);
 		int i;
-		for (i = 0; i < size; i++) {
+		for (i = 0; i < chunk_size; i++) {
 			data[i] = '1';
 		}
 
+	        gettimeofday(&startTime, NULL);
+                uint64_t times = 0;
 		long long int total;
-		for (total = 0; total < 2 * GB;) {
-                        rc = blobfs_file_write(wfile, data, total, size);
+		for (total = 0; total < bench_size * GB;) {
+                        rc = blobfs_file_write(wfile, data, total, chunk_size);
                         if (rc != 0) {
                                 fprintf(stderr, "ERR: blobfs write file %s\n", filename);
 				goto writeout;
                         }
-			total += size;
+                        times++;
+			total += chunk_size;
                         rc = blobfs_file_sync(wfile);
                         if (rc != 0) {
                                 fprintf(stderr, "ERR: blobfs sync file %s\n", filename);
@@ -105,28 +118,35 @@ delout:
 			goto writeout;
                 }
 writeout:
-                ftime(&endTime);
-                fprintf(stdout, "write 10GB file escaped: %d ms\n", (endTime.time-startTime.time)*1000 + (endTime.millitm - startTime.millitm));
+                gettimeofday(&endTime, NULL);
+                float escaped = (endTime.tv_sec - startTime.tv_sec) * S + (endTime.tv_usec - startTime.tv_usec);
+                fprintf(stdout, "Write: %d GB file\nChunk size: %ld \nEscaped time: %f ms\nWrite times: %ld\nIOPS: %f\nlatency: %f us\nOutput: %f MB\n",
+                    bench_size, chunk_size, escaped/MS, times, times/(escaped/S), escaped/times, bench_size*GB/(escaped/S)/MB);
+                //fprintf(stdout, "GB: %d MB: %d KB: %d US: %d MS: %d S: %d", GB, MB, KB, US, MS, S);
                 unmount_blobfs();
                 exit(0);
 	}
 
+	gettimeofday(&startTime, NULL);
 	if (strcmp(argv[4], "read") == 0) {
                 blobfs_file *rfile = NULL;
 		char *filename = "write_data";
-		char buf[size];
+                uint64_t chunk_size = (uint64_t) atol(argv[5]) * KB;
+                char *buf = (char *) malloc(chunk_size);
                 rc = blobfs_open_file(filename, SPDK_BLOBFS_OPEN_CREATE, &rfile);
                 if (rc != 0) {
 			fprintf(stderr, "ERR: blobfs open file %s\n", filename);
 			goto readout;
 		}
 
+                uint64_t times = 0;
                 int64_t filesize = blobfs_file_get_length(rfile);
                 int64_t offset = 0;
                 uint64_t read_len = 0;
                 do {
                        read_len = blobfs_file_read(rfile, buf, offset, 4*1024);
-                       fprintf(stdout, "read size: %d read offset: %ld filesize: %ld\n", read_len, offset, filesize);
+                       times++;
+                       //fprintf(stdout, "read size: %d read offset: %ld filesize: %ld\n", read_len, offset, filesize);
                        offset += read_len;
                 } while (filesize != offset);
                 rc = blobfs_file_close(rfile);
@@ -136,18 +156,36 @@ writeout:
                 }
 readout:
 
-                ftime(&endTime);
-                fprintf(stdout, "read  %dGB file escaped: %d ms\n", filesize/GB, (endTime.time-startTime.time)*1000 + (endTime.millitm - startTime.millitm));
+                gettimeofday(&endTime, NULL);
+                float escaped = (endTime.tv_sec - startTime.tv_sec) * S + (endTime.tv_usec - startTime.tv_usec);
+                fprintf(stdout, "Read: %d GB file\nChunk size: %ld \nEscaped time: %f ms\nRead times: %ld\nIOPS: %f\nlatency: %f us\nInput: %f MB\n",
+                    bench_size, chunk_size, escaped/MS, times, times/(escaped/S), escaped/times, bench_size*GB/(escaped/S)/MB);
+                //uint64_t escaped = (endTime.tv_sec - startTime.tv_sec) * S + (endTime.tv_usec - startTime.tv_usec);
+                //fprintf(stdout, "read  %d GB chunk size: %d file escaped: %d ms\n", bench_size, size, escaped/MS );
                 unmount_blobfs();
                 exit(0);
 	}
 
 	if (strcmp(argv[4], "clear") == 0) {
-        rc = blobfs_delete_file(argv[5]);
+                blobfs_file_name *all_files = NULL;
+                rc = blobfs_list_all_files(&all_files);
                 if (rc != 0) {
-                        fprintf(stderr, "ERR: blobfs delete file %s\n", argv[5]);
-			exit(-1);
+                        fprintf(stderr, "ERR: blobfs list all file \n");
+                        goto clean;
                 }
+                blobfs_file_name_ptr it = all_files;
+                fprintf(stdout, "blobfs: files: ");
+                while(it != NULL) {
+                        fprintf(stdout, "to clear file %s ", it->name);
+                        rc = blobfs_delete_file(it->name);
+                        if (rc != 0) {
+                                fprintf(stderr, "ERR: blobfs delete file %s\n", argv[5]);
+			        exit(-1);
+                        }
+                        it = it->next;
+                }
+                free_blobfs_file_name(all_files);
+                fprintf(stdout, "\n");
                 unmount_blobfs();
                 exit(0);
 	}
@@ -251,8 +289,9 @@ close_wfile:
                 fprintf(stderr, "ERR: blobfs close write file %s\n", filename);
                 goto exit;
         }
-	ftime(&endTime);
-	fprintf(stdout, "%d ms\n", (endTime.time-startTime.time)*1000 + (endTime.millitm - startTime.millitm));
+	gettimeofday(&endTime, NULL);
+        uint64_t escaped = (endTime.tv_sec - startTime.tv_sec) * S + (endTime.tv_usec - startTime.tv_usec);
+	fprintf(stdout, "%d ms\n", escaped/MS);
         int times = 9;
         while (times) {
                 fprintf(stdout, "blobfs: to list all files \n");
@@ -284,8 +323,9 @@ clean:
 exit:
         free_blobfs_file_stat(file_stat);
         file_stat = NULL;
-	ftime(&endTime);
-	fprintf(stdout, "%d ms\n", (endTime.time-startTime.time)*1000 + (endTime.millitm - startTime.millitm));
+	gettimeofday(&endTime, NULL);
+        escaped = (endTime.tv_sec - startTime.tv_sec) * S + (endTime.tv_usec - startTime.tv_usec);
+	fprintf(stdout, "%d ms\n", escaped/MS);
         unmount_blobfs();
         fprintf(stdout, "blobfs: exit!!\n");
 
